fe0daf492af5d7f290dc8912966ee467f1ba1453
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-21 08:45:15 -0400

    Create hello.py

---
+++ PythonTraining/hello.py
@@ -0,0 +1,9 @@
+print ("Hello")
+
+name = input("Enter your name : ")
+print("Hello", name)
+print("Hello", name, "How are you today")
+
+end_val = '(@#)\n'
+print("Hello", name, "How are you today", sep='-', end=end_val)
+print('I\'m good Thanks!', end=end_val)
