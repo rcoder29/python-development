b961475a4c10d20e02bcbfed47336da510cd4877
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-21 08:48:19 -0400

    Create typessample.py

---
+++ PythonTraining/typessample.py
@@ -0,0 +1,11 @@
+# print types of following expressions
+
+print(type(5))
+print(type(5000*50000000))
+print(type('j'))
+print(type(3.14*5000000000000000000))
+print(type(1j))
+print(type((1,)))
+print(type([1]))
+print(type({'a':'b'}))
+print(type(None))
