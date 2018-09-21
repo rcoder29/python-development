87e363c4449094c85869f06d384b03691b970f7d
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-21 08:46:50 -0400

    Update osenviron.py

--- PythonTraining/osenviron.py
+++ PythonTraining/osenviron.py
@@ -2,3 +2,17 @@ import os
 
 # read a variable from OS environment and print it
 print("PATH is", os.environ["PATH"])
+
+# Retrieve current working directory (`cwd`)
+cwd = os.getcwd()
+print(cwd)
+
+# Change directory
+os.chdir("C:\\temp")
+cwd = os.getcwd()
+print("New Working Directory - ", cwd)
+
+# List all files and directories in current directory
+filelist = os.listdir('.')
+for file in filelist:
+    print(file)
