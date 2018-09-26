3ce1ebfd327447b2d6ec88f199a7377deb14f672
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-26 09:16:33 -0400

    Update fibonacci.py

--- interview-coding/problemsolving/fibonacci.py
+++ interview-coding/problemsolving/fibonacci.py
@@ -4,4 +4,15 @@
 2. Given a number find if it is a Fibonacci number
 3. do 1 & 2 using Recursive mechanism
 
+Regular, Recursive & using Generators
+
+	- Make sure candidate understands what Fibonacci sequence is
+	- Print a function that generates Fibonacci sequence up to 100
+	- Do versions of the method with 
+		○ Regular
+		○ Recursive
+		○ Using Generator
+	
+CODE
+
 '''
