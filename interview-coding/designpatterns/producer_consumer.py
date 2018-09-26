cacf42e7074987e19569efe19cee1652ef84c5fd
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-26 09:18:45 -0400

    Create producer_consumer.py

---
+++ interview-coding/designpatterns/producer_consumer.py
@@ -0,0 +1,8 @@
+'''
+
+Implement Producer, Consumer, Queue classes and handle the messaging between them so when there is new message in Queue the value gets read and printed and waits otherwise!
+
+	1. Understanding of messaging platform - point to point, Message Queue, Message Bus
+  2.Threading & Synchronization Issues handling
+  
+  '''
