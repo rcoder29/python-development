a9366f16c0cc0fa378e4c76e2ef10cc9b6c1fe5f
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-26 09:07:55 -0400

    Create staircaserecursive.py

---
+++ interview-coding/problemsolving/staircaserecursive.py
@@ -0,0 +1,6 @@
+''' Find a different paths to climb a stair case
+Input - Number of steps to claim
+        Combination steps possible to take {1,2,3}, means from each step either one or two or three steps can be claimed in one go
+        
+Output = List of sets outlinining the steps possible to get to the top of the stair case'''
+
