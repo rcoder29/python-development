5f0f205762ed218a413ee90a649389b358484d14
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-26 09:18:00 -0400

    Update connectionpool.py

--- interview-coding/designpatterns/connectionpool.py
+++ interview-coding/designpatterns/connectionpool.py
@@ -1,6 +1,8 @@
-	- Implement a Class that acts as a Pool for Resource Connections
+'''	- Implement a Class that acts as a Pool for Resource Connections
 	- Provide ability to get, return, close
 	- In background has ability to check health on it periodically and auto connect
 	- Handle Exceptions, to make sure there is no resource lead etc.,
 
 CODE
+
+'''
