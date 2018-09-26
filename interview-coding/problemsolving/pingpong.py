2eb93af39646d49eca7d5815a34074140859aa61
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-26 09:16:14 -0400

    Create pingpong.py

---
+++ interview-coding/problemsolving/pingpong.py
@@ -0,0 +1,12 @@
+Write code to implement Ping Pong
+
+	- Write a function that prints "Ping Pong"
+	- Print Ping if input number is divisible by 3
+	- Print Pong if input number is divisible by 5
+	- Print Ping Pong if the number is divisible by both 3 and 5
+
+
+Goals - 
+	1. Shows demonstration of basic comfort with Python coding
+	2. Conditionals
+Mod operator
