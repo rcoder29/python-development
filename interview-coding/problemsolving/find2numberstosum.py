afee8446b4f5725c858a7b9ced17fe69bc398502
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-26 09:06:13 -0400

    Create find2numberstosum.py

---
+++ interview-coding/problemsolving/find2numberstosum.py
@@ -0,0 +1,8 @@
+''' given list of numbers find two numbers in the list whose sum equals to a sum value provided
+  Input - List of numbers
+      - Sum number to match with
+  Output - Null if none matches
+           two numbers and their index when sum matches '''
+           
+           
+  
