a9c842e73d5358d6b44996c25e70fbf1ef01d22b
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-26 09:17:15 -0400

    Update find2numberstosum.py

--- interview-coding/problemsolving/find2numberstosum.py
+++ interview-coding/problemsolving/find2numberstosum.py
@@ -2,7 +2,19 @@
   Input - List of numbers
       - Sum number to match with
   Output - Null if none matches
-           two numbers and their index when sum matches '''
+           two numbers and their index when sum matches 
+           
+  	- Given list of numbers, find combination of two numbers that equal to the sum given
+	- [1,4,8,20,10] and sum is 5
+
+	1. Come up with algorithm to solve the problem
+	2. Demonstrate understanding of the problem
+	3. Come up with initial working solution
+	4. Revise for performance in different iterations
+	5. Gives ability to test the candidate on performance notation
+	6. Once a good performing solution is arrived at, implement the same in language of choice (Python?)
+
+           '''
            
            
   
