4c7e7eaa8db6afb2af7eda0b1c3ad1a768e52373
rvr <16635983+rcoder29@users.noreply.github.com>
2018-09-29 08:03:22 -0400

    Create readme.py

---
+++ interview-coding/problemsolving/test/readme.py
@@ -0,0 +1,13 @@
+Python Testing Frameworks & reference materials
+
+All options overview
+http://pythontesting.net/start-here/
+
+starter tutorial for pytest
+https://semaphoreci.com/community/tutorials/testing-python-applications-with-pytest
+
+
+CI Integration Options
+Bamboo integrated with Bitbuket
+SemphoreCI integrated with Bitbucket
+Jenkins integrated with Bitbucket
